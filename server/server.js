f1516d7bc375a8b83ac656be4a2729016735b828
daigza09 <daigza09@gmail.com>
2024-02-15 18:28:57 -0600

    testing

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require('dotenv').config()
+
+const express = require("express")
+// express app 
+const app = express()
+
+//middleware
+app.use((req, res, next) => {
+    console.log(req.path, req.method)
+})
+
+// setting up route handeler
+app.get('/', (req, res) =>{
+    res.json({mssg: 'Welcome to the app'})
+})
+// listen for requests
+PORT = 8080
+app.listen(process.env.PORT, () =>{
+    console.log('listening on port', process.env.PORT)
+})
+
+process.env
