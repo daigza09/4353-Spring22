d99908ca09158c644b44c4d138c9c069baaef111
daigza09 <daigza09@gmail.com>
2024-02-18 14:56:29 -0600

    changed background color

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,11 @@
 function Footer() {
-    return (
-      <footer className="bg-[#05204A] text-white font-semibold p-4 py-10">
-        <div className="container mx-auto">
-          <div className="flex justify-between">
-              <p>Testing</p>
-          </div>
-        </div>
-      </footer>
-    )
-  }
-  
-  export default Footer
+  return (
+    <footer className="text-white font-semibold p-4 py-10 fixed bottom-0 w-full">
+      <div className="container mx-auto">
+        <p>Testing</p>
+      </div>
+    </footer>
+  )
+}
+
+export default Footer;
