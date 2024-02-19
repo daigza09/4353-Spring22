fa4204f456907a051002d716eb2b4b4ee5921964
daigza09 <daigza09@gmail.com>
2024-02-19 15:32:18 -0600

    adding buttons

--- client/src/pages/FuelForm.jsx
+++ client/src/pages/FuelForm.jsx
@@ -1,9 +1,22 @@
 import { Link } from "react-router-dom";
 import React, { useState } from "react";
 function FuelForm(){
+    const [gasLocation, setGasLocation] = useState('');
+
+    const handleGasLocationChange = (e) => {
+        setGasLocation(e.target.value);
+    };
     return(
-        <main className = "relative h-screen bg-cover">
-            <div>Gas Location</div>
+        <main className="flex flex-col items-center justify-center h-screen bg-cover">
+            <div className = "flex flex-col items-left">
+            <label htmlFor="gasLocation">Gas Location:</label>
+            <input
+                type="text"
+                id="gasLocation"
+                value={gasLocation}
+                onChange={handleGasLocationChange}
+            />
+            </div>
             <div>Fuel Type</div>
             <div>Number of Gallons</div>
             <div>Purchase Date</div>
@@ -11,7 +24,7 @@ function FuelForm(){
             <div>Delivery Date</div>
             <div>Price Per Gallon</div>
             <div>Total</div>
-            <div></div>
+            <div>Order Button</div>
         </main>
     );
 }
