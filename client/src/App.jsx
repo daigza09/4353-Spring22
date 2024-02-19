21a67cf6ec77316d62a1bd48fd6fa595162c4133
daigza09 <daigza09@gmail.com>
2024-02-18 19:43:53 -0600

    testing

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,7 +4,7 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/LogIn";
 import Signup from "./pages/Signup";
-import FQform from "./pages/FQform";
+import FormQ from "./pages/FormQ";
 import './App.css';
 
 function App() {
@@ -17,7 +17,7 @@ function App() {
         <Route path="Contact" element={<Contact/>}/>
         <Route path="Login" element={<Login/>}/>
         <Route path="Signup" element={<Signup/>}/>
-        <Route path="FQform" element ={<FQform/>}/>
+        <Route path="FormQ" element ={<FormQ/>}/>
       </Route>
     </Routes>
     </>
