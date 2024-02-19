f88c15f326b29ed5552946b2657b8336b3daa830
mmdessie4 <mmdessie4@gmail.com>
2024-02-18 22:42:29 -0600

    App.jsx modified

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,6 +1,8 @@
 import { Routes, Route, Navigate } from "react-router-dom";
 import Layout from "./layout/Layout";
 import Home from "./pages/Home";
+import Signup from "./pages/Signup";
+import Login from "./pages/Login";
 import './App.css';
 
 function App() {
@@ -10,6 +12,8 @@ function App() {
       <Route path="/" element={<Layout />}>
         <Route index element={<Navigate to="/home" />} />
         <Route path="home" element={<Home />}/>
+        <Route path="Signup" element={<Signup/>}/>
+        <Route path="Login" element={<Login/>}/>
       </Route>
     </Routes>
     </>
