56944affbb0ab8d8acb97c078e0adec5a2caf6cb
Jynxae <realkhalily@hotmail.com>
2024-02-18 19:40:32 -0600

    set up history page

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,22 +4,24 @@ import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/LogIn";
 import Signup from "./pages/Signup";
-import './App.css';
+import History from "./pages/History";
+import "./App.css";
 
 function App() {
   return (
     <>
-    <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Navigate to="/home" />} />
-        <Route path="home" element={<Home />}/>
-        <Route path="Contact" element={<Contact/>}/>
-        <Route path="Login" element={<Login/>}/>
-        <Route path="Signup" element={<Signup/>}/>
-      </Route>
-    </Routes>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Navigate to="/home" />} />
+          <Route path="home" element={<Home />} />
+          <Route path="Contact" element={<Contact />} />
+          <Route path="Login" element={<Login />} />
+          <Route path="Signup" element={<Signup />} />
+          <Route path="History" element={<History />} />
+        </Route>
+      </Routes>
     </>
-  )
+  );
 }
 
 export default App;
---
+++ client/src/pages/History.jsx
@@ -0,0 +1,10 @@
+import { Link } from "react-router-dom";
+function History() {
+  return (
+    <main>
+      <h1>This is the history page</h1>
+      <h2>Testing </h2>
+    </main>
+  );
+}
+export default History;
