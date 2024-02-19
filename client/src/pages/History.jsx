9c70438c03e7d7ff529ead81c9381adc0b98f0bc
Jynxae <realkhalily@hotmail.com>
2024-02-19 16:44:24 -0600

    Adjusting History page, adding few more things in

--- client/src/pages/History.jsx
+++ client/src/pages/History.jsx
@@ -6,22 +6,32 @@ function History() {
   const [data, setData] = useState(Data);
   return (
     <main>
-      <h1>Testing page</h1>
       <div className="flex items-center justify-center">
         <table className="w-11/12 border-collapse">
           <thead className="border-solid border-2 border-gray bg-white text-left">
-            <th className="py-2 px-5 text-lg">Delivery Date</th>
-            <th className="py-2 px-5 text-lg">Amount</th>
-            <th className="py-2 px-5 text-lg">Item</th>
-            <th className="py-2 px-5 text-lg">Gallons Requested</th>
+            <tr>
+              <th className="py-2 px-5 text-lg text-black">Delivery Date</th>
+              <th className="py-2 px-5 text-lg text-black">Amount</th>
+              <th className="py-2 px-5 text-lg text-black">Item</th>
+              <th className="py-2 px-5 text-lg text-black">
+                Gallons Requested
+              </th>
+            </tr>
           </thead>
           <tbody>
-            {data.map((current) => (
-              <tr className="border-solid border-2 border-gray bg-white text-left">
-                <td className="py-4 px-5 text-lg">{current.DeliveryDate}</td>
-                <td className="py-4 px-5 text-lg">${current.Amount}</td>
-                <td className="py-4 px-5 text-lg">{current.Item}</td>
-                <td className="py-4 px-5 text-lg">
+            {data.map((current, index) => (
+              <tr
+                key={index}
+                className="border-solid border-2 border-gray bg-white text-left"
+              >
+                <td className="py-4 px-5 text-lg text-black">
+                  {current.DeliveryDate}
+                </td>
+                <td className="py-4 px-5 text-lg text-black">
+                  ${current.Amount}
+                </td>
+                <td className="py-4 px-5 text-lg text-black">{current.Item}</td>
+                <td className="py-4 px-5 text-lg text-black">
                   {current.GallonsRequested}
                 </td>
               </tr>
