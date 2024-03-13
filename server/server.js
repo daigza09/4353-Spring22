f9728df2e90ecee68d47d49e7e08029219e2a18e
daigza09 <daigza09@gmail.com>
2024-03-13 13:35:12 -0500

    testing

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const dotenv = require('dotenv').config();
 const express = require('express');
 const {errorHandler} = require('./middlewares/errorMiddleware');
-const connectDB = require('./config/db');
+const connectDB = require('./config/db')
 PORT = 8080;
 
 connectDB()
