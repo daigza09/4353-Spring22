9055a86a97a6b0fe7b3d2f4b6f1e404cb511d924
daigza09 <daigza09@gmail.com>
2024-03-13 12:20:22 -0500

    working on backend setup

--- client/src/pages/FuelForm.jsx
+++ client/src/pages/FuelForm.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 
-
+//import axios from "axios";
 function FuelForm() {
     const [formData, setFormData] = useState({
         gasLocation: '',
