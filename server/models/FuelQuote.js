2cc7122c00a6debd9ebcd05decc909b4988eb1cb
daigza09 <daigza09@gmail.com>
2024-03-14 14:20:21 -0500

    testing before pushing to main

--- server/models/FuelQuote.js
+++ server/models/FuelQuote.js
@@ -5,12 +5,12 @@ const Schema = mongoose.Schema;
 const fuelQuoteSchema = new Schema({
   gasLocation: { type: String, enum: ['01-TX', '02-FL', '03-NY'], default: '' },
   fuelType: { type: String, enum: ['Diesel', 'Gasoline'], default: '' },
-  numGallons: { type: String, default: '' },
-  purchaseDate: { type: String, default: '' },
-  pricePerGallon: { type: String, default: '' },
-  deliveryDate: { type: String, default: '' },
+  numGallons: { type: Number, default: 0 }, // Change data type to Number
+  purchaseDate: { type: Date, default: Date.now }, // Use Date type for dates
+  pricePerGallon: { type: Number, default: 0 }, // Change data type to Number
+  deliveryDate: { type: Date, default: Date.now }, // Use Date type for dates
   deliveryAddress: { type: String, default: '' },
-  total: { type: String, default: '' },
+  total: { type: Number, default: 0 }, // Change data type to Number
 });
 
 module.exports = mongoose.model('FuelQuote', fuelQuoteSchema);
