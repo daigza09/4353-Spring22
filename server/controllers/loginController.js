2c25c57c1f3cd9557375c549e6b839ce662ec61b
mmdessie4 <mmdessie4@gmail.com>
2024-03-21 15:18:10 -0500

    Sign up and login testing added

--- server/controllers/loginController.js
+++ server/controllers/loginController.js
@@ -4,7 +4,6 @@ const Signup = require('../models/Signup');
 exports.login = asyncHandler(async (req, res) => {
     const { email, password } = req.body;
 
-    // Check if email and password are provided
     if (!email || !password) {
         return res.status(400).json({ error: 'Please provide email and password' });
     }
@@ -12,7 +11,6 @@ exports.login = asyncHandler(async (req, res) => {
     try {
         const user = await Signup.findOne({ email });
 
-        // Check if user exists
         if (!user) {
             throw new Error('User not found');
         }
@@ -21,10 +19,9 @@ exports.login = asyncHandler(async (req, res) => {
             throw new Error('Invalid email or password');
         }
 
-        // Respond with user details
         res.status(200).json({ message: 'Login successful', user });
     } catch (error) {
-        console.error(error); // Log error for debugging
+        console.error(error); 
         res.status(401).json({ error: error.message });
     }
 });
