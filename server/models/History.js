18e892d4a3a60369cba2b52919481dce8b4068c8
Jynxae <realkhalily@hotmail.com>
2024-04-11 15:57:35 -0500

    adjusted history page to pull from database

--- server/models/History.js
+++ server/models/History.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const historySchema = new Schema({
+  email: {type: String, default: ''},
   fuelType: { type: String, enum: ['Diesel', 'Gasoline'], default: '' },
   numGallons: { type: Number, default: 0 }, // Change data type to Number
   purchaseDate: { type: Date, default: Date.now }, // Use Date type for dates
