ba01049c1668a43b02d88cf1e039c5261e3b8d6e
daigza09 <daigza09@gmail.com>
2024-04-25 23:30:53 -0500

    finished pricing module

--- server/controllers/quoteController.js
+++ server/controllers/quoteController.js
@@ -122,7 +122,7 @@ const getPrice = asyncHandler(async(req, res) => {
     const margin = currentPPG * (locationFactor - historyFactor + gallonsFactor + companyProfit);
     const suggestedPPG = currentPPG + margin;
 
-    const sugTotal = suggestedPPG * numGallons;
+    const sugTotal = (suggestedPPG * numGallons).toFixed(2);
     res.status(200).json({
       message: 'Price calculated successfully',
       margin,
